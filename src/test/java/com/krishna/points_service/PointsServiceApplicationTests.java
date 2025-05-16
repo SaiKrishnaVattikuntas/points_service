c2cc6047bb045af80a2595d0bcfe32bded687345
Sai Krishna Vattikunta <krishnavattikuntas@gmail.com>
2025-05-16 00:04:06 -0400

    Initial Commit

---
+++ src/test/java/com/krishna/points_service/PointsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.krishna.points_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PointsServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
